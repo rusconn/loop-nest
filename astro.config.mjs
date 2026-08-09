baa5147f167e3970d89b608c24534cce3be3e17a
rusconn <rusconn3@gmail.com>
2026-08-09 10:46:18 +0900

    change(ui): unify pwa theme color to app background color

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,8 +27,8 @@ export default defineConfig({
         start_url: "/",
         display: "standalone",
         orientation: "portrait",
-        background_color: "#222",
-        theme_color: "#222",
+        background_color: "#111",
+        theme_color: "#111",
         icons: [
           {
             src: "/pwa-icon-192.png",
